e458a97f058285acb36fe7893801b237b47627e2
yinzhen <zhen.yin@100credit.com>
2020-07-09 21:54:27 +0800

    事务

--- src/main/java/com/zhenzhen/demo/order/controller/OrderController.java
+++ src/main/java/com/zhenzhen/demo/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.zhenzhen.demo.order.controller;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,7 @@ public class OrderController {
 	}
 	
 	@RequestMapping("/getUserInfoRibbon")
+	@Transactional
 	public String getUserInfoRibbon() {
 		return restTemplate.getForObject("http://USER/getUser", String.class);
 	}
