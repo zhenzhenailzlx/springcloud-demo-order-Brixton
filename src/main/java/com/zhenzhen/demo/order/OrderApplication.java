edf0bb9ec2c96e2f77ab26635b7590a5d2b3eca7
zhen.yin <zhen.yin@100credit.com>
2020-10-31 16:41:07 +0800

    修改

--- src/main/java/com/zhenzhen/demo/order/OrderApplication.java
+++ src/main/java/com/zhenzhen/demo/order/OrderApplication.java
@@ -1,13 +1,12 @@
 package com.zhenzhen.demo.order;
 
+import com.zhenzhen.demo.order.util.HttpClientUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import com.zhenzhen.demo.order.util.HttpClientUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
